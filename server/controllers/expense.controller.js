dd3e6627e73bc340c370c7cb402e353a9a1ee867
Michle99 <sammiyonyx@gmail.com>
2023-11-25 18:01:08 -0500

    Added create expense API and mounted it to express server.

---
+++ server/controllers/expense.controller.js
@@ -0,0 +1,19 @@
+import Expense from '../models/expense.model.js';
+import extend from 'lodash/extend';
+import errorHandler from './../helpers/dbErrorHandler';
+import mongoose from 'mongoose';
+
+const create = async (req, res) => {
+    try {
+      req.body.recorded_by = req.auth._id
+      const expense = new Expense(req.body)
+      await expense.save()
+      return res.status(200).json({
+        message: "Expense recorded!"
+      })
+    } catch (err) {
+      return res.status(400).json({
+        error: errorHandler.getErrorMessage(err)
+      })
+    }
+}
