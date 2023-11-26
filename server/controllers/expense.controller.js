00e8058ee9ef3b899d29de8490f6c874d637ce02
Michle99 <sammiyonyx@gmail.com>
2023-11-25 19:51:13 -0500

    Updated expense controller & router, and added expense folder comp.

--- server/controllers/expense.controller.js
+++ server/controllers/expense.controller.js
@@ -17,3 +17,5 @@ const create = async (req, res) => {
       })
     }
 }
+
+export default { create };
--- server/routes/expense.routes.js
+++ server/routes/expense.routes.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import authCtrl from '../controllers/auth.controller';
-
+import expenseCtrl from '../controllers/expense.controller';
 
 const router = express.Router();
 
 router.route('/api/expenses')
     .post(authCtrl.requireSignin, expenseCtrl.create);
+
+    
+export default router;
