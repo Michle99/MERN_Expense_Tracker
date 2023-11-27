764e0a41e00e1354a6696d07eb629c93d373adad
Michle99 <sammiyonyx@gmail.com>
2023-11-26 20:52:53 -0500

    Added expense put and delete routes

--- server/routes/expense.routes.js
+++ server/routes/expense.routes.js
@@ -8,5 +8,11 @@ router.route('/api/expenses')
     .post(authCtrl.requireSignin, expenseCtrl.create)
     .get(authCtrl.requireSignin, expenseCtrl.listByUser)
 
+router.route('/api/expenses/:expenseId')
+    .put(authCtrl.requireSignin, expenseCtrl.hasAuthorization, expenseCtrl.update)
+    .delete(authCtrl.requireSignin, expenseCtrl.hasAuthorization, expenseCtrl.remove)
+
+router.param('expenseId', expenseCtrl.expenseByID)
+
     
 export default router;
