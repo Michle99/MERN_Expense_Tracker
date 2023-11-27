41ff14e4f70b4eda0f504e842b1e7cd4fe022003
Michle99 <sammiyonyx@gmail.com>
2023-11-26 21:18:39 -0500

    Added expense controllers and routes

--- server/routes/expense.routes.js
+++ server/routes/expense.routes.js
@@ -4,20 +4,20 @@ import expenseCtrl from '../controllers/expense.controller';
 
 const router = express.Router();
 
-// router.route('/api/expenses/current/preview')
-//   .get(authCtrl.requireSignin, expenseCtrl.currentMonthPreview)
+router.route('/api/expenses/current/preview')
+  .get(authCtrl.requireSignin, expenseCtrl.currentMonthPreview)
 
-// router.route('/api/expenses/by/category')
-//   .get(authCtrl.requireSignin, expenseCtrl.expenseByCategory)
+router.route('/api/expenses/by/category')
+  .get(authCtrl.requireSignin, expenseCtrl.expenseByCategory)
 
-// router.route('/api/expenses/plot')
-//   .get(authCtrl.requireSignin, expenseCtrl.plotExpenses)
+router.route('/api/expenses/plot')
+  .get(authCtrl.requireSignin, expenseCtrl.plotExpenses)
 
-// router.route('/api/expenses/category/averages')
-//   .get(authCtrl.requireSignin, expenseCtrl.averageCategories)
+router.route('/api/expenses/category/averages')
+  .get(authCtrl.requireSignin, expenseCtrl.averageCategories)
 
-// router.route('/api/expenses/yearly')
-//   .get(authCtrl.requireSignin, expenseCtrl.yearlyExpenses)
+router.route('/api/expenses/yearly')
+  .get(authCtrl.requireSignin, expenseCtrl.yearlyExpenses)
 
 router.route('/api/expenses')
     .post(authCtrl.requireSignin, expenseCtrl.create)
